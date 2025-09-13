ea65add33417e53731fd72fe31c53dedfaea0180
MERCY NNAMCHI <mercynnamani123@gmail.com>
2025-09-13 22:56:06 +0100

    Add simple interest calculatot script

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+principal = 1000  # Principal amount in dollars
+rate = 0.05         # Annual interest rate in percent
+time = 3           # Time in years  
+# Calculate simple interest
+interest = principal * rate * time
+# Calculate total amount after interest
+#print result
+print(f"The simple interest is:", interest)
+
